61045df95434d0d2d5cbb2b8da790f1897a5f498
Carlos Manrique Enguita <Manri98@users.noreply.github.com>
2016-11-10 16:23:20 +0100

    .

---
+++ selectfolder.h
@@ -0,0 +1,29 @@
+ifndef SELECTFOLDER_H
+#define SELECTFOLDER_H
+
+#include <QAbstractItemModel>
+
+class SelectFolder : public QAbstractItemModel
+{
+    Q_OBJECT
+
+public:
+    explicit SelectFolder(QObject *parent = 0);
+
+    // Header:
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    // Basic functionality:
+    QModelIndex index(int row, int column,
+                      const QModelIndex &parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex &index) const override;
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+private:
+};
+
+#endif // SELECTFOLDER_H
